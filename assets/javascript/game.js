6e761967b20016bb456d4cf15910133a81b3065c
Dev Jain <devrjain32200@gmail.com>
2019-05-31 10:03:09 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,67 @@
-var choices = ["a", "b", "c","d", "e", "f"];
+var choices = ["a", "b", "c","d", "e", "f", "g", "h", "i","j", "k", "l","m", "n", "o","p", "q", "r","s", "t", "u","v", "w", "x", "y", "z"];
 var user = document.getElementById("user-guess");
-var random = document.getElementById("computer-guess");
+// var random = document.getElementById("computer-guess");
 var outcome = document.getElementById("outcome");
+var guesses = document.getElementById("user-guess-arr");
 var win = document.getElementById("w");
 var loss = document.getElementById("l");
-var tie = document.getElementById("t");
+var livesLeft = document.getElementById("lives-left")
 var wi = 0;
-var lo = 10;
+var lo = 0;
+var lives = 10;
+var userGuessArr = [];
+var randomGuess = choices[Math.floor(Math.random() * choices.length)];
+console.log("Computer Guess: " + randomGuess);
 
 document.onkeyup = function(event) {
   var userGuess = event.key;
-  var randomGuess = choices[Math.floor(Math.random() * choices.length)];
+  userGuessArr.push(userGuess);
+  guesses.textContent = userGuessArr.join(", ");
   user.textContent = event.key;
-  random.textContent = randomGuess;
 
   if (randomGuess == userGuess){
     console.log("You win!");
     outcome.textContent = "You win!";
-    win.textContent = (wi=wi+1);
+    wi++;
+    win.textContent = wi;
+    // reset the game
+    //  - choose another randomGuess
+    //  - empty out the user guesses array
+    //  - On the page, clear out the 
+    //  - reset the lifes to 10
+    randomGuess = choices[Math.floor(Math.random() * choices.length)];
+    userGuessArr = [];
+    guesses.textContent = userGuessArr;
+    lives = 10;
+    livesLeft.textContent = lives;
   }
-  else (randomGuess !== userGuess); {
-    console.log("You lose!");
-    outcome.textContent = "Lose!";
-    loss.textContent = (lo=lo-1);
-  } 
-  
+  else {
+    // decrement the lives by 1
+    // lives = lives - 1
+    lives--;
+    livesLeft.textContent = lives;
+    // if lives === 0, they lost, and we need to tick up lo by 1 restart the game
+    if (lives === 0) {
+      console.log("You lose!");
+      outcome.textContent = "You lose!";
+      lo++;
+      loss.textContent = lo;
+      // reset the game
+      //  - choose another randomGuess
+      //  - empty out the user guesses array
+      //  - reset the lives to 10
+      randomGuess = choices[Math.floor(Math.random() * choices.length)];
+      userGuessArr = [];
+      guesses.textContent = userGuessArr;
+      lives = 10;
+      livesLeft.textContent = lives;
+    }
+    else {
+      outcome.textContent = "Guess again!";
+    }
+    
+
+    // otherwise, keep playing
+      
+  }
 }
